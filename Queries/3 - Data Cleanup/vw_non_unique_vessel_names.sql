47ac4ea76af2e4e15fc0750974d579d5d33ba01c
George Apostolakis <40721266+jorgen82@users.noreply.github.com>
2025-03-24 14:45:07 +0200

    Create vw_non_unique_vessel_names.sql

---
+++ Queries/3 - Data Cleanup/vw_non_unique_vessel_names.sql
@@ -0,0 +1,15 @@
+/*  This view is to be used to find vessels that have same vessel name ans mssi
+    We might want to filter out those vessels on our analysis, since out goal is to match the fixtures with the vessels / trajectories etc
+    and the match can only be done using the vessel names
+*/
+
+CREATE OR REPLACE VIEW ais.vw_non_unique_vessel_names AS
+SELECT vessel_name 
+FROM (
+    SELECT vessel_name, imo
+    FROM ais.vessel
+	--WHERE vessel_category <> 'Tankers' 
+    GROUP BY vessel_name, imo
+    ) 
+GROUP BY vessel_name 
+HAVING count(*)>1;
