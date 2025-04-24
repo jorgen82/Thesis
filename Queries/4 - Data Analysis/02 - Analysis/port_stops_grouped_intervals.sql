0a7dbda8fd3e8fe64d0bc7d69794c04e7ec18dcf
George Apostolakis <40721266+jorgen82@users.noreply.github.com>
2025-04-24 17:16:59 +0300

    Add files via upload

---
+++ Queries/4 - Data Analysis/02 - Analysis/port_stops_grouped_intervals.sql
@@ -0,0 +1,17 @@
+/* Day apart between consecutive identified port calls. */
+
+WITH time_diff AS (
+	SELECT *
+		,CAST(COALESCE(EXTRACT(EPOCH FROM (ts_begin - LAG(ts_begin) OVER (PARTITION BY vessel_id, port_id ORDER BY ts_begin)))/86400, 0) as int) as time_diff_days
+		,COALESCE(EXTRACT(EPOCH FROM (ts_begin - LAG(ts_end) OVER (PARTITION BY vessel_id, port_id ORDER BY ts_begin)))/3600, 0) AS non_stop_hours
+	   FROM data_analysis.port_stops
+)
+
+SELECT time_diff_days as "Time Difference (Days)", count, "pct over total"
+FROM (
+	select time_diff_days, count(*) as count, CAST(100 * (count(*)/ sum(count(*)) over ()) as decimal(5,2)) as "pct over total"
+	FROM time_diff
+	GROUP BY time_diff_days
+) x
+WHERE time_diff_days > 0
+ORDER BY time_diff_days
