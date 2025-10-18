82e3c381bae44c6ddeae816547e05115ef37df35
George Apostolakis <40721266+jorgen82@users.noreply.github.com>
2025-10-18 10:59:19 +0300

    Update waiting_areas analysis.sql

--- Queries/4 - Data Analysis/02 - Analysis/Waiting Areas/waiting_areas analysis.sql
+++ Queries/4 - Data Analysis/02 - Analysis/Waiting Areas/waiting_areas analysis.sql
@@ -1,5 +1,6 @@
 
-WITH base AS (
+/* We investigate which areas are utilized the most, based on Areas stops and Waiting Hours */
+WITH base AS (  -- Fetch basic stats for the traffic on waiting areas belonging to a specific port (in this case Galveston, you may change it to investigate another area)
     SELECT p.port_name,
         t.waiting_areas_cluster_id,
         CAST(SUM(t.waiting_minutes)/60 AS DECIMAL(8,2)) AS total_waiting_hours,
@@ -11,19 +12,19 @@ WITH base AS (
     WHERE p.port_name = 'GALVESTON'   -- Change the port to the one you would like to investigate
     GROUP BY p.port_name, t.waiting_areas_cluster_id
 ),
-pct AS (
+pct AS ( -- total stops and total hours of each waiting area, but the totals of the specific port (pct of an area over the port where it belongs to)
     SELECT *,
-        CAST(total_stops * 1.0 / SUM(total_stops) OVER (PARTITION BY port_name) AS DECIMAL(5,4)) AS total_stops_pct,
+        CAST(total_stops * 1.0 / SUM(total_stops) OVER (PARTITION BY port_name) AS DECIMAL(5,4)) AS total_stops_pct, 
         CAST(total_waiting_hours * 1.0 / SUM(total_waiting_hours) OVER (PARTITION BY port_name) AS DECIMAL(5,4)) AS total_waiting_hours_pct
     FROM base
 ),
-cumulative AS (
+cumulative AS ( -- commulative total stops and waiting areas, in descenting order. This will make the investigation easier (eg we want to see which areas are used to server the 80% of the traffic)
     SELECT *,
         SUM(total_stops_pct) OVER (PARTITION BY port_name ORDER BY total_stops DESC) AS stops_cumulative_pct,
         SUM(total_waiting_hours_pct) OVER (PARTITION BY port_name ORDER BY total_waiting_hours DESC) AS waiting_hours_cumulative_pct
     FROM pct
 ),
-cumulative_compare AS (
+cumulative_compare AS (  -- a comparison to see how much this cummulative number is changing. This will help in cases you would like to investigate areas which will make small difference (eg less than 10% of change in port stops)
     SELECT *,
         CAST((stops_cumulative_pct - LAG(stops_cumulative_pct) OVER (PARTITION BY port_name ORDER BY total_stops DESC)) / LAG(stops_cumulative_pct) OVER (PARTITION BY port_name ORDER BY total_stops DESC)  as DECIMAL(5,2)) as stops_cumulative_pct_increase,
         CAST((waiting_hours_cumulative_pct - LAG(waiting_hours_cumulative_pct) OVER (PARTITION BY port_name ORDER BY total_waiting_hours DESC)) / LAG(waiting_hours_cumulative_pct) OVER (PARTITION BY port_name ORDER BY total_waiting_hours DESC)  as DECIMAL(5,2)) as waiting_hours_cumulative_pct_increase
@@ -35,28 +36,16 @@ FROM cumulative_compare
 ORDER BY total_stops DESC;
 
 
-
-
-
--- Tracks to Galveston Area ports
-SELECT DISTINCT t.track_id, tr.track
-FROM data_analysis.waiting_areas_traffic t
-INNER JOIN data_analysis.tracks tr on tr.id = t.track_id
-INNER JOIN context_data.ports p on p.id = t.to_port_id
---WHERE p.port_name in ('GALVESTON', 'HOUSTON', 'DEER PARK', 'PASADENA', 'SINCO', 'NORSWORTHY', 'BAYTOWN', 'TEXAS CITY')
-WHERE p.port_name in ('CORPUS CHRISTI', 'PORT INGLESIDE', 'PORT ARANSAS', 'ROCKPORT')
-
--- Waiting Areas
-SELECT t.waiting_areas_cluster_id, wa.concave_hull, CAST((SUM(t.waiting_minutes) / count(t.vessel_id)) / 60 as decimal(9,3)) as avg_waiting_hours, count(*) as total_tracks
+-- Concave Hulls of Waiting Areas in specific region (filtered by ports)
+SELECT t.waiting_areas_cluster_id, CAST((SUM(t.waiting_minutes) / count(t.vessel_id)) / 60 as decimal(9,3)) as avg_waiting_hours, count(*) as total_tracks, wa.concave_hull
 FROM data_analysis.waiting_areas_traffic t
 INNER JOIN data_analysis.waiting_areas wa on wa.cid_dbscan = t.waiting_areas_cluster_id
 INNER JOIN context_data.ports p on p.id = t.to_port_id
---WHERE p.port_name in ('GALVESTON', 'HOUSTON', 'DEER PARK', 'PASADENA', 'SINCO', 'NORSWORTHY', 'BAYTOWN', 'TEXAS CITY')
-WHERE p.port_name in ('CORPUS CHRISTI', 'PORT INGLESIDE', 'PORT ARANSAS', 'ROCKPORT')
+WHERE p.port_name in ('GALVESTON', 'HOUSTON', 'DEER PARK', 'PASADENA', 'SINCO', 'NORSWORTHY', 'BAYTOWN', 'TEXAS CITY')
 GROUP BY t.waiting_areas_cluster_id, wa.concave_hull
 
 
--- Metrics for vessel heading to Galveston Area
+-- Metrics for vessel heading to Galveston Area. Used in Section 5.3.5)
 with galveston_areas as (SELECT wa.cid_dbscan
     FROM data_analysis.waiting_areas wa
     INNER JOIN data_analysis.ports_voronoi pv ON ST_Within(wa.centr, pv.voronoi_zone)
@@ -144,4 +133,3 @@ INNER JOIN context_data.ports p on p.id = t.to_port_id
 WHERE p.port_name in ('CORPUS CHRISTI', 'PORT INGLESIDE', 'PORT ARANSAS', 'ROCKPORT')
     AND t.from_port_stops_grouped_id IS NOT NULL
     AND waiting_areas_cluster_id not in (select cid_dbscan from galveston_areas)
-
