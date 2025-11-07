2e45883f46b79f32000a25efc61bb9eb9e61ea5b
George Apostolakis <40721266+jorgen82@users.noreply.github.com>
2025-11-07 14:11:04 +0200

    Fix typo in SQL view comment

--- Queries/3 - Data Cleanup/1 - vw_non_unique_vessel_names.sql
+++ Queries/3 - Data Cleanup/1 - vw_non_unique_vessel_names.sql
@@ -1,4 +1,4 @@
-/*  This view is to be used to find vessels that have same vessel name ans mssi
+/*  This view is to be used to find vessels that have same vessel name and mssi
     We might want to filter out those vessels on our analysis, since out goal is to match the fixtures with the vessels / trajectories etc
     and the match can only be done using the vessel names
 */
